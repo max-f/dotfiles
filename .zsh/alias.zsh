d23dcedc9cbae5dd6d0063cb3d97ab13f2364dc6
Max <omg.plz@gmx.net>
2011-04-20 00:40:49 +0200

    git aliases added

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -4,8 +4,6 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias pacman='sudo pacman'
-
 alias ls='ls -lh --color=auto'
 alias lsd='ls -d *(-/N)'      # list visible directories
 alias lsf='ls *(-.N)'         # list visible files
@@ -25,6 +23,9 @@ alias cpma='/opt/quake3/ioquake3.x86_64 +sef fs_game cpma'
 #alias cpma='/usr/local/games/quake3/quake3 +set fs_game cpma'
 #alias bp='sshfs keks@brechpunkt.de:/home/keks -o nonempty ~/brechpunkt/'
 
+# pacman
+alias pacman='sudo pacman'
+
 alias pacs='pacman -Ss'
 alias paci='pacman -S'
 alias pacqi='pacman -Qi'
@@ -36,9 +37,63 @@ alias pacu='pacman -Syu'
  
 alias S='sudo'
 
+# dropbox
 alias dropup="sh $HOME/devel/bash/dropbox.sh up"
 alias dropdown="sh $HOME/devel/bash/dropbox.sh down"
 
+# git (c&p from oh-my-zsh)
+# Aliases
+alias g='git'
+compdef g=git
+alias gst='git status'
+compdef _git gst=git-status
+alias gl='git pull'
+compdef _git gl=git-pull
+alias gup='git fetch && git rebase'
+compdef _git gup=git-fetch
+alias gp='git push'
+compdef _git gp=git-push
+gdv() { git-diff -w "$@" | view - }
+compdef _git gdv=git-diff
+alias gc='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
+alias gco='git checkout'
+compdef _git gco=git-checkout
+alias gb='git branch'
+compdef _git gb=git-branch
+alias gba='git branch -a'
+compdef _git gba=git-branch
+alias gcount='git shortlog -sn'
+compdef gcount=git
+alias gcp='git cherry-pick'
+compdef _git gcp=git-cherry-pick
+alias glg='git log --stat --max-count=5'
+compdef _git glg=git-log
+
+# Git and svn mix
+alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
+compdef git-svn-dcommit-push=git
+
+#
+# Will return the current branch name
+# Usage example: git pull origin $(current_branch)
+#
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+echo ${ref#refs/heads/}
+}
+
+# these aliases take advantage of the previous function
+alias ggpull='git pull origin $(current_branch)'
+compdef ggpull=git
+alias ggpush='git push origin $(current_branch)'
+compdef ggpush=git
+alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
+compdef ggpnp=git
+
+# handling certain filetypes
 alias -s {mp3,avi,mpeg,mpg,ogg}=mplayer
 alias -s {htm,html}=$BROWSER
 alias -s {c,cpp,py,java,cfg}=$EDITOR
