73d5bbc8fc28a0b41d28f9db12a26909ff09a122
Max <maaax.fischer@gmail.com>
2023-02-20 00:19:50 +0100

    Add coloring of cursor line and listing of trailing spaces

--- .config/nvim/lua/max/set.lua
+++ .config/nvim/lua/max/set.lua
@@ -30,5 +30,10 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+
+-- show these 'invisible' chars
+vim.opt.list = true
+vim.opt.listchars:append({ trail = '·' })
 
 vim.g.mapleader = ","
