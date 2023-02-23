6a6b78f3b25cd0589c1d6ab6ea58ddb975799886
Max Fischer <max.fischer@diva-e.com>
2023-02-23 17:11:03 +0100

    Update ipython config and add trouble plugin to neovim to show problems in a list

--- .config/nvim/lua/max/packer.lua
+++ .config/nvim/lua/max/packer.lua
@@ -59,4 +59,22 @@ return require('packer').startup(function(use)
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
