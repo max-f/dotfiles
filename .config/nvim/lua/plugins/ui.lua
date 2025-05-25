233e3c18eb2e488a87ae0a94cbe43062bfea9c59
Max Fischer <maaax.fischer@gmail.com>
2025-05-25 16:46:11 +0200

    Some additional colorscheme which is not too loud

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -56,6 +56,15 @@ local M = {
 	'p00f/alabaster.nvim',
 	'gmr458/cold.nvim',
 	'chrsm/paramount-ng.nvim',
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			-- NOTE: you do not need to call setup if you don't want to.
+			require("vague").setup({
+				-- optional configuration here
+			})
+		end
+	},
 }
 
 return M
