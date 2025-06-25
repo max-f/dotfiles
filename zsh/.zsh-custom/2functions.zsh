1a27a078bd5f5503f5ee295d15cd1d0d505b1371
Max Fischer <maaax.fischer@gmail.com>
2025-06-25 15:09:54 +0200

    Add workaround regarding clipboard/zsh-vi-mode interaction

--- zsh/.zsh-custom/2functions.zsh
+++ zsh/.zsh-custom/2functions.zsh
@@ -209,3 +209,70 @@ function yy() {
 	fi
 	rm -f -- "$tmp"
 }
+
+
+# For better clipboard integration with zsh-vi-mode (https://github.com/jeffreytse/zsh-vi-mode/issues/19)
+## helper functions
+cbread() {
+	xclip -selection primary -i -f | xclip -selection secondary -i -f | xclip -selection clipboard -i
+}
+
+cbprint() {
+    if   x=$(xclip -o -selection clipboard 2> /dev/null); then
+      echo -n $x
+    elif x=$(xclip -o -selection primary   2> /dev/null); then
+      echo -n $x
+    elif x=$(xclip -o -selection secondary 2> /dev/null); then
+      echo -n $x
+    fi
+}
+
+# keybindings for zsh-vi-mode
+my_zvm_vi_yank() {
+  zvm_vi_yank
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_delete() {
+  zvm_vi_delete
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_change() {
+  zvm_vi_change
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_change_eol() {
+  zvm_vi_change_eol
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_put_after() {
+  CUTBUFFER=$(cbprint)
+  zvm_vi_put_after
+  zvm_highlight clear # zvm_vi_put_after introduces weird highlighting for me
+}
+
+my_zvm_vi_put_before() {
+  CUTBUFFER=$(cbprint)
+  zvm_vi_put_before
+  zvm_highlight clear # zvm_vi_put_before introduces weird highlighting for me
+}
+
+zvm_after_lazy_keybindings() {
+  zvm_define_widget my_zvm_vi_yank
+  zvm_define_widget my_zvm_vi_delete
+  zvm_define_widget my_zvm_vi_change
+  zvm_define_widget my_zvm_vi_change_eol
+  zvm_define_widget my_zvm_vi_put_after
+  zvm_define_widget my_zvm_vi_put_before
+
+  zvm_bindkey visual 'y' my_zvm_vi_yank
+  zvm_bindkey visual 'd' my_zvm_vi_delete
+  zvm_bindkey visual 'x' my_zvm_vi_delete
+  zvm_bindkey vicmd  'C' my_zvm_vi_change_eol
+  zvm_bindkey visual 'c' my_zvm_vi_change
+  zvm_bindkey vicmd  'p' my_zvm_vi_put_after
+  zvm_bindkey vicmd  'P' my_zvm_vi_put_before
+}
